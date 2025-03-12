e0f7b92422fa5f974c76b53e90756d4a3a80fc4c
sergei_darin <swd86@mail.ru>
2025-03-12 18:41:07 +0400

    убрал инициализацию переменной List<Illness> illnesses в классе Person.java

--- src/main/java/ru/darin/nutrition_recommendation/model/Person.java
+++ src/main/java/ru/darin/nutrition_recommendation/model/Person.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,6 +29,6 @@ public class Person {
             inverseJoinColumns = @JoinColumn(name = "illness_id")
     )
 
-    private List<Illness> illnesses = new ArrayList<>();
+    private List<Illness> illnesses;
 
 }
