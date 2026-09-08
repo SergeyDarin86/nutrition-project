3fde13b0c6233c03c5ad03f792e0639a215cdef8
sergei_darin <swd86@mail.ru>
2026-09-08 21:31:07 +0400

    Добавил аннотации в классы PersonDTO, ProtocolDTO для создания документации

--- src/main/java/ru/darin/nutrition_recommendation/dto/PersonDTO.java
+++ src/main/java/ru/darin/nutrition_recommendation/dto/PersonDTO.java
@@ -1,5 +1,7 @@
 package ru.darin.nutrition_recommendation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.darin.nutrition_recommendation.util.validation.NutritionValidation;
 
@@ -8,12 +10,17 @@ import java.util.UUID;
 
 @Data
 @NutritionValidation
+@Schema(description = "Класс, для ввода/отображения данных о человеке.")
 public class PersonDTO {
 
     private UUID personId;
 
+    @JsonProperty("fullName")
+    @Schema(description = "ФИО", example = "Иванов Андрей Михайлович")
     private String fullName;
 
+    @JsonProperty("protocols")
+    @Schema(description = "Протоколы")
     private List<ProtocolDTO> protocols;
 
 }
--- src/main/java/ru/darin/nutrition_recommendation/dto/ProtocolDTO.java
+++ src/main/java/ru/darin/nutrition_recommendation/dto/ProtocolDTO.java
@@ -1,5 +1,7 @@
 package ru.darin.nutrition_recommendation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.darin.nutrition_recommendation.util.validation.NutritionValidation;
 
@@ -7,10 +9,13 @@ import java.util.UUID;
 
 @Data
 @NutritionValidation
+@Schema(description = "Класс, для ввода/отображения данных о протоколе питания.")
 public class ProtocolDTO {
 
     private UUID protocolId;
 
+    @JsonProperty("protocolTitle")
+    @Schema(description = "Название протокола", example = "ЭРД")
     private String protocolTitle;
 
 }
